70b12ea182430880e0e361d5f8bdaffe95c76653
Jum-99 <jum3a.k5@gmail.com>
2025-03-19 19:29:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 print("Die API läuft erfolgreich!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Die API läuft erfolgreich!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
